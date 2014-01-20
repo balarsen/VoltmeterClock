c198910163e162f8a666e24e13d1aee3a790848d
Brian Larsen <balarsen@gmail.com>
2014-01-19 17:37:59 -0700

    cleanup using arduino functions

--- VoltmeterClock/VoltmeterClock/VoltmeterClock.ino
+++ VoltmeterClock/VoltmeterClock/VoltmeterClock.ino
@@ -5,40 +5,35 @@
  
 RTC_DS1307 RTC;
 
+const uint8_t hourPin   = 5; // these should be revisited to see what is acutaully best
+const uint8_t minutePin = 6;
+
 void makeISOdate(String *isodate, DateTime dt);
 
 char isotime[19];
 
-
-float hour2val(int hour) {
-   return hour/24. * 5. ;  // scale the hour to 1 then 5 volts
-}
-
-float minute2val(int minute) {
-  return minute/60. * 5;  // scale the hour to 1 then 5 volts
-}
-
-
 void setup () {
+    // set pins as outputs:
+  pinMode(minutePin, OUTPUT);
+  pinMode(hourPin, OUTPUT);
     Serial.begin(57600);
     Wire.begin();
  
    if (RTC.isrunning()) {
-    Serial.println("RTC is running in setup.");
+    Serial.println(F("RTC is running in setup."));
    }
  
   if (! RTC.isrunning()) {
-    Serial.println("RTC is NOT running!");
+    Serial.println(F("RTC is NOT running!"));
     // following line sets the RTC to the date & time this sketch was compiled
     RTC.adjust(DateTime(__DATE__, __TIME__));
   }
 
     RTC.begin();
     DateTime now = RTC.now();
-  if  (now.year() == 2000) { // it wasnot set properly
+  if  (now.year() == 2000) { // it wasn't set properly
         RTC.adjust(DateTime(__DATE__, __TIME__));
   }
- 
 }
 
 void makeISOdate(char *isodate, DateTime dt){
@@ -46,15 +41,24 @@ void makeISOdate(char *isodate, DateTime dt){
       dt.year(), dt.month(), dt.day(), dt.hour(), dt.minute(), dt.second() );
 }
  
+ 
+ uint8_t hourVal;
+ uint8_t minuteVal;
+ 
 void loop () {
     DateTime now = RTC.now();
    makeISOdate(isotime, now);
     Serial.println(isotime);
-   Serial.print(hour2val(now.hour()));
-   Serial.print("  --  ");
-   Serial.println(minute2val(now.minute()));
-   
+    hourVal = map(now.hour(), 0, 23, 0, 255);
+    minuteVal = map(now.minute(), 0, 59, 0, 255);
+   Serial.print(hourVal);
+   Serial.print(F("  --  "));
+   Serial.println(minuteVal);
+      
+   analogWrite(hourPin, hourVal);
+   analogWrite(minutePin, minuteVal);
+
    
     Serial.println();
-    delay(3000);
+    delay(5000);
 }
